89c7eedb70b3f3e02062e055bf66aa98c003c041
hys <21301039@bjtu.edu.cn>
2024-04-24 22:37:35 +0800

    初始commit

---
+++ src/main/java/com/java/warehousemanagementsystem/WarehouseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.java.warehousemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarehouseManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/warehousemanagementsystem/WarehouseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.warehousemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
